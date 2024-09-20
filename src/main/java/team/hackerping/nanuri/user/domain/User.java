b96d6785f267b1c1e9fe5075e2e4649c94952ce0
sanghee0820 <lsh277604@gmail.com>
2024-09-21 00:55:20 +0900

    feat: User 정보 추가

--- src/main/java/team/hackerping/nanuri/user/domain/User.java
+++ src/main/java/team/hackerping/nanuri/user/domain/User.java
@@ -2,22 +2,47 @@ package team.hackerping.nanuri.user.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
 @Table(name = "MEMBER")
-@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
-
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "USER_ID")
     private Long id;
 
     @NotNull
     private String username;
+
     @NotNull
     private String encodedPassword;
+
     @NotNull
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus;
+
+    private String profileImageUrl;
+
+    private User(String username, String encodedPassword, UserType userType) {
+        this.username = username;
+        this.encodedPassword = encodedPassword;
+        this.userType = userType;
+        this.userStatus = UserStatus.BLOCKED;
+    }
+
+    public static User of(String username, String encodedPassword, UserType userType) {
+        return new User(username, encodedPassword, userType);
+    }
+
+    public void uploadProfileImage(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
 }
