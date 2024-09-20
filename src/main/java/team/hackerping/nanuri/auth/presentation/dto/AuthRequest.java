bd827b6f3c4f39d726191feaa6702ca56fed2ad9
sanghee0820 <lsh277604@gmail.com>
2024-09-21 00:57:17 +0900

    feat: toCommand 매서드 추가

--- src/main/java/team/hackerping/nanuri/auth/presentation/dto/AuthRequest.java
+++ src/main/java/team/hackerping/nanuri/auth/presentation/dto/AuthRequest.java
@@ -1,6 +1,7 @@
 package team.hackerping.nanuri.auth.presentation.dto;
 
 import org.springframework.web.multipart.MultipartFile;
+import team.hackerping.nanuri.auth.application.dto.command.AuthCommand;
 import team.hackerping.nanuri.user.domain.UserType;
 
 public class AuthRequest {
@@ -9,5 +10,9 @@ public class AuthRequest {
             String password,
             UserType userType,
             MultipartFile enrollmentProofImage
-    ) { }
+    ) {
+        public AuthCommand.Signup toCommand() {
+            return AuthCommand.Signup.of(username, password, userType, enrollmentProofImage);
+        }
+    }
 }
