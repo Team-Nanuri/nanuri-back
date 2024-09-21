d874d234c17cc0c838325c76cd069192704bcc41
sanghee0820 <lsh277604@gmail.com>
2024-09-21 15:22:22 +0900

    feat: Jwt 발급 로직 추가

--- src/main/java/team/hackerping/nanuri/security/config/HandlerConfig.java
+++ src/main/java/team/hackerping/nanuri/security/config/HandlerConfig.java
@@ -2,6 +2,7 @@ package team.hackerping.nanuri.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import team.hackerping.nanuri.security.application.JwtService;
 import team.hackerping.nanuri.security.handler.AccessHandler;
 import team.hackerping.nanuri.security.handler.AuthenticationHandler;
 
@@ -14,7 +15,9 @@ public class HandlerConfig {
     }
 
     @Bean
-    public AuthenticationHandler authenticationHandler() {
-        return new AuthenticationHandler();
+    public AuthenticationHandler authenticationHandler(
+            JwtService jwtService
+    ) {
+        return new AuthenticationHandler(jwtService);
     }
 }
--- src/main/java/team/hackerping/nanuri/security/handler/AuthenticationHandler.java
+++ src/main/java/team/hackerping/nanuri/security/handler/AuthenticationHandler.java
@@ -1,29 +1,48 @@
 package team.hackerping.nanuri.security.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import team.hackerping.nanuri.security.application.JwtService;
+import team.hackerping.nanuri.security.application.dto.PrincipleDetails;
+import team.hackerping.nanuri.security.application.dto.TokenDetails;
 
 @Slf4j
+@RequiredArgsConstructor
 public class AuthenticationHandler implements AuthenticationSuccessHandler, AuthenticationFailureHandler {
+    private final JwtService jwtService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
-        //TODO: JWT 토큰 발급
-        log.info("Authentication Success");
+        var principleDetails = (PrincipleDetails) authentication.getPrincipal();
+        var tokens = jwtService.generateLoginToken(principleDetails.getClaims());
+
+        setTokenResponse(response, tokens);
+    }
+
+    private void setTokenResponse(HttpServletResponse response, TokenDetails tokens)
+            throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        response.getWriter().write(objectMapper.writeValueAsString(tokens));
+        response.getWriter().flush();
     }
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
                                         AuthenticationException exception) throws IOException, ServletException {
-        //TODO: 인증 실패 처리
-        log.info("Authentication Failure");
+        log.error("Authentication failed: {}", exception.getMessage());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
