a27d948358cf70280c0cba76e80d6cc03f21aa4a
HyeonJeong <wgwjh05169@gmail.com>
2024-09-22 01:56:28 +0900

    feat: 내가 좋아하는 게시글 중 타겟에 해당하는 좋아요가 있는지 확인

--- src/main/java/team/hackerping/nanuri/article/application/LikeService.java
+++ src/main/java/team/hackerping/nanuri/article/application/LikeService.java
@@ -1,8 +1,15 @@
 package team.hackerping.nanuri.article.application;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
+import team.hackerping.nanuri.article.domain.Article;
 import team.hackerping.nanuri.article.persistence.LikeRepository;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
@@ -12,5 +19,23 @@ public class LikeService {
 
     public Boolean searchLike(Long userId, Long articleId) {
         return likeRepository.existsByUserIdAndArticleId(userId, articleId);
+    }
+
+    public List<Pair<Article, Boolean>> searchLikes(Long userId, List<Article> articles) {
+        Set<Long> likes = new HashSet<>(
+                likeRepository.findAllByUserId(userId).stream()
+                    .map(like -> like.getArticle().getId())
+                    .toList());
+
+        List<Pair<Article, Boolean>> result = new ArrayList<>();
+        for (Article article : articles) {
+            if (likes.contains(article.getId())) {
+                result.add(Pair.of(article, true));
+            } else {
+                result.add(Pair.of(article, false));
+            }
+        }
+
+        return result;
     }
 }
