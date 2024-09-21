313f25ca18135cce49f496f7dcfea45cb31db5ce
HyeonJeong <wgwjh05169@gmail.com>
2024-09-22 01:52:09 +0900

    feat: 동적 쿼리를 활용한 레포지토리 작성

---
+++ src/main/java/team/hackerping/nanuri/article/persistence/ArticleJpaQueryRepository.java
@@ -0,0 +1,66 @@
+package team.hackerping.nanuri.article.persistence;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+import team.hackerping.nanuri.article.application.command.PagingArticleCommand;
+import team.hackerping.nanuri.article.domain.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class ArticleJpaQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public Page<Article> pagingByCriteria(PagingArticleCommand command) {
+        List<Article> query = queryFactory
+                .select(QArticle.article)
+                .from(QArticle.article)
+                .where(
+                        inTitle(command.getKeyword()),
+                        eqWriterId(command.getWriterId()),
+                        inCategories(command.getCategories()),
+                        eqShareType(command.getShareType()),
+                        eqStatus(command.getStatus())
+                )
+                .offset(command.getPageable().getOffset())
+                .limit(command.getPageable().getPageSize())
+                .fetch();
+
+        Long total = queryFactory
+                .select(QArticle.article.count())
+                .from(QArticle.article)
+                .where(
+                        inTitle(command.getKeyword())
+                )
+                .fetchOne();
+
+        return new PageImpl<>(query, command.getPageable(), total == null ? 0 : total);
+    }
+
+    private BooleanExpression inTitle(String title) {
+        return StringUtils.hasText(title) ? QArticle.article.title.in(title) : null;
+    }
+
+    private BooleanExpression eqWriterId(Long writerId) {
+        return writerId != null ? QArticle.article.writer.id.eq(writerId) : null;
+    }
+
+    private BooleanExpression inCategories(List<ItemCategory> categories) {
+        return categories != null ? QArticle.article.itemCategory.in(categories) : null;
+    }
+
+    private BooleanExpression eqShareType(ShareType shareType) {
+        return shareType != null ? QArticle.article.shareType.eq(shareType) : null;
+    }
+
+    private BooleanExpression eqStatus(ArticleStatus status) {
+        return status != null ? QArticle.article.status.eq(status) : null;
+    }
+}
