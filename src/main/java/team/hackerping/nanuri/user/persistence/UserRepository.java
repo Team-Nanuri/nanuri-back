b35b7ca5f3e56182e8f0a7f1a9c92836e0ca5aa9
sanghee0820 <lsh277604@gmail.com>
2024-09-21 14:41:40 +0900

    feat: username으로 유저 반환 기능 추가

--- src/main/java/team/hackerping/nanuri/user/persistence/UserRepository.java
+++ src/main/java/team/hackerping/nanuri/user/persistence/UserRepository.java
@@ -1,9 +1,11 @@
 package team.hackerping.nanuri.user.persistence;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import team.hackerping.nanuri.user.domain.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
