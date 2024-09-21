84a4f6ae32246b572e35d98c034f6a8a6bf3b8d4
HyeonJeong <wgwjh05169@gmail.com>
2024-09-21 22:13:39 +0900

    refactor: selfValidating 수행할 때 NanuriException 발생시키도록 수정

--- src/main/java/team/hackerping/nanuri/global/application/SelfValidating.java
+++ src/main/java/team/hackerping/nanuri/global/application/SelfValidating.java
@@ -1,9 +1,13 @@
 package team.hackerping.nanuri.global.application;
 
 import jakarta.validation.*;
+import lombok.extern.slf4j.Slf4j;
+import team.hackerping.nanuri.global.exception.NanuriException;
+import team.hackerping.nanuri.global.exception.code.GeneralError;
 
 import java.util.Set;
 
+@Slf4j
 public abstract class SelfValidating<T> {
 
     private final Validator validator;
@@ -18,7 +22,13 @@ public abstract class SelfValidating<T> {
         Set<ConstraintViolation<T>> violations = validator.validate((T)this);
 
         if (!violations.isEmpty()) {
-            throw new ConstraintViolationException(violations);
+            String path = violations
+                    .stream()
+                    .map(violation -> violation.getPropertyPath() + ": " + violation.getMessage())
+                    .reduce((s1, s2) -> s1 + ", " + s2)
+                    .orElse("");
+
+            throw new NanuriException(GeneralError.CONSTRAINT_VIOLATION, path);
         }
     }
 }
