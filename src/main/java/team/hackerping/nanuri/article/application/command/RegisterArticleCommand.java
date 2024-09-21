5bf8c14fc5bc7820829f0beed04a96248f76ab2c
HyeonJeong <wgwjh05169@gmail.com>
2024-09-21 21:30:38 +0900

    rename: DTO 패키지 만들어 이동

--- src/main/java/team/hackerping/nanuri/article/application/command/RegisterArticleCommand.java
+++ src/main/java/team/hackerping/nanuri/article/application/command/RegisterArticleCommand.java
@@ -1,4 +1,4 @@
-package team.hackerping.nanuri.article.application;
+package team.hackerping.nanuri.article.application.command;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
