be9de0bbbdb4172fed11bbeacf042377218208d5
HyeonJeong <wgwjh05169@gmail.com>
2024-09-22 01:50:18 +0900

    chore: JpaConfig에 JPAQueryFactory 빈 등록

---
+++ src/main/java/team/hackerping/nanuri/global/jpa/JpaConfig.java
@@ -0,0 +1,22 @@
+package team.hackerping.nanuri.global.jpa;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+@RequiredArgsConstructor
+public class JpaConfig {
+
+    private final EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+
+}
