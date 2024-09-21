098b354d15b841a887c8d4b1099d5bdce92b0cd0
HyeonJeong <wgwjh05169@gmail.com>
2024-09-21 21:08:39 +0900

    refactor: Handler가 직접 반환하도록 수정

--- src/main/java/team/hackerping/nanuri/global/exception/WebExceptionHandler.java
+++ src/main/java/team/hackerping/nanuri/global/exception/WebExceptionHandler.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.*;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,16 +15,18 @@ import team.hackerping.nanuri.global.exception.code.WebError;
 
 import java.util.List;
 
+@Slf4j
 @RestControllerAdvice
-public class ExceptionFormatHandler {
+public class WebExceptionHandler {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public void handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        NanuriException ex;
 
         var cause = e.getCause();
 
         if (cause instanceof JsonParseException)
-            throw new NanuriException(WebError.ILLEGAL_JSON);
+            ex = new NanuriException(WebError.ILLEGAL_JSON);
 
         if (cause instanceof JsonMappingException) {
             var path = buildPath((JsonMappingException) cause);
@@ -30,24 +35,26 @@ public class ExceptionFormatHandler {
                 Class<?> targetType = ((InvalidFormatException) cause).getTargetType();
 
                 if (targetType.isEnum())
-                    throw new NanuriException(WebError.ILLEGAL_JSON, buildEnumMessage(targetType, path));
+                    ex = new NanuriException(WebError.ILLEGAL_JSON, buildEnumMessage(targetType, path));
                 else
-                    throw new NanuriException(WebError.ILLEGAL_JSON, path + "은(는) " + targetType.getSimpleName() + "이어야 합니다.");
+                    ex = new NanuriException(WebError.ILLEGAL_JSON, path + "은(는) " + targetType.getSimpleName() + "이어야 합니다.");
             }
 
             if (cause instanceof MismatchedInputException) {
                 Class<?> targetType = ((MismatchedInputException) cause).getTargetType();
 
                 if (targetType != null)
-                    throw new NanuriException(WebError.ILLEGAL_JSON, path + "을(를) " + targetType.getSimpleName() + "에 매핑할 수 없습니다.");
+                    ex = new NanuriException(WebError.ILLEGAL_JSON, path + "을(를) " + targetType.getSimpleName() + "에 매핑할 수 없습니다.");
                 else
-                    throw new NanuriException(WebError.ILLEGAL_JSON, path + "을(를) 매핑하는 데 실패하였습니다.");
+                    ex = new NanuriException(WebError.ILLEGAL_JSON, path + "을(를) 매핑하는 데 실패하였습니다.");
             }
 
-            throw new NanuriException(WebError.ILLEGAL_JSON, "JSON 매핑에 실패하였습니다.");
+            ex = new NanuriException(WebError.ILLEGAL_JSON, "JSON 매핑에 실패하였습니다.");
         }
 
-        throw new NanuriException(WebError.NOT_READABLE_HTTP_MESSAGE);
+        ex = new NanuriException(WebError.NOT_READABLE_HTTP_MESSAGE);
+
+        return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(ErrorResponse.from(ex));
     }
 
     private String buildPath(JsonMappingException e) {
@@ -81,22 +88,35 @@ public class ExceptionFormatHandler {
     }
 
     @ExceptionHandler(ServletRequestBindingException.class)
-    public void handleServletRequestBindingException(ServletRequestBindingException e) {
+    public ResponseEntity<ErrorResponse> handleServletRequestBindingException(ServletRequestBindingException e) {
+        NanuriException ex;
+
         if (e instanceof MissingServletRequestParameterException)
-            throw new NanuriException(WebError.MISSING_REQUEST_PARAMETER, ((MissingServletRequestParameterException) e).getParameterName());
+            ex = new NanuriException(WebError.MISSING_REQUEST_PARAMETER, ((MissingServletRequestParameterException) e).getParameterName());
 
         if (e instanceof MissingPathVariableException)
-            throw new NanuriException(WebError.MISSING_PATH_VARIABLE, ((MissingPathVariableException) e).getVariableName());
+            ex = new NanuriException(WebError.MISSING_PATH_VARIABLE, ((MissingPathVariableException) e).getVariableName());
 
         if (e instanceof MissingRequestHeaderException)
-            throw new NanuriException(WebError.MISSING_REQUEST_HEADER, ((MissingRequestHeaderException) e).getHeaderName());
+            ex = new NanuriException(WebError.MISSING_REQUEST_HEADER, ((MissingRequestHeaderException) e).getHeaderName());
 
         if (e instanceof MissingRequestCookieException)
-            throw new NanuriException(WebError.MISSING_REQUEST_COOKIE, ((MissingRequestCookieException) e).getCookieName());
+            ex = new NanuriException(WebError.MISSING_REQUEST_COOKIE, ((MissingRequestCookieException) e).getCookieName());
 
         if (e instanceof MissingMatrixVariableException)
-            throw new NanuriException(WebError.MISSING_MATRIX_VARIABLE, ((MissingMatrixVariableException) e).getVariableName());
+            ex = new NanuriException(WebError.MISSING_MATRIX_VARIABLE, ((MissingMatrixVariableException) e).getVariableName());
 
-        throw new NanuriException(WebError.SERVLET_ERROR);
+        ex = new NanuriException(WebError.SERVLET_ERROR);
+
+        return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(ErrorResponse.from(ex));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        FieldError fieldError = e.getBindingResult().getFieldError();
+
+        NanuriException ex = new NanuriException(WebError.INVALID_ARGUMENT, fieldError.getField());
+
+        return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(ErrorResponse.from(ex));
     }
 }
