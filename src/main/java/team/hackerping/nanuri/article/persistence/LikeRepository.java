41e2f6e7dfa7604ac7418ec7df5478782a7b8ef6
HyeonJeong <wgwjh05169@gmail.com>
2024-09-22 01:53:50 +0900

    feat: 레포지토리에 내가 좋아요 한 게시글 조회하는 메소드 작성

--- src/main/java/team/hackerping/nanuri/article/persistence/LikeRepository.java
+++ src/main/java/team/hackerping/nanuri/article/persistence/LikeRepository.java
@@ -3,6 +3,10 @@ package team.hackerping.nanuri.article.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.hackerping.nanuri.article.domain.Like;
 
+import java.util.List;
+
 public interface LikeRepository extends JpaRepository<Like, Long> {
     Boolean existsByUserIdAndArticleId(Long userId, Long articleId);
+
+    List<Like> findAllByUserId(Long userId);
 }
