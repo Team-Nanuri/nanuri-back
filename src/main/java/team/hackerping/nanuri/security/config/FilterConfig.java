af24742c433555cbb564efa845738a7a4d9c37de
sanghee0820 <lsh277604@gmail.com>
2024-09-22 11:45:20 +0900

    feat: Cors 필터 적용

--- src/main/java/team/hackerping/nanuri/security/config/FilterConfig.java
+++ src/main/java/team/hackerping/nanuri/security/config/FilterConfig.java
@@ -2,6 +2,9 @@ package team.hackerping.nanuri.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import team.hackerping.nanuri.security.application.JwtProvider;
 import team.hackerping.nanuri.security.filter.JwtAuthorizationFilter;
 
@@ -11,5 +14,19 @@ public class FilterConfig {
     @Bean
     public JwtAuthorizationFilter jwtAuthorizationFilter(JwtProvider jwtProvider) {
         return new JwtAuthorizationFilter(jwtProvider);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        var source = new UrlBasedCorsConfigurationSource();
+        var config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
 }
