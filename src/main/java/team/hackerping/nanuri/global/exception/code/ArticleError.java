70b1b0f5fc454b368542652639adabc9e7c94809
HyeonJeong <wgwjh05169@gmail.com>
2024-09-22 14:17:06 +0900

    feat: ArticleError 에러 추가

--- src/main/java/team/hackerping/nanuri/global/exception/code/ArticleError.java
+++ src/main/java/team/hackerping/nanuri/global/exception/code/ArticleError.java
@@ -11,7 +11,11 @@ public enum ArticleError implements ErrorCode {
     ILLEGAL_DATE(HttpStatus.BAD_REQUEST, "A005","대여 종료일자가 대여 시작일자보다 빠를 수 없습니다."),
     ILLEGAL_CATEGORY_SEARCH(HttpStatus.BAD_REQUEST, "A006","유효하지 않은 카테고리 값으로 검색을 시도했습니다."),
 
-    ALREADY_LIKED(HttpStatus.BAD_REQUEST, "A200", "이미 좋아요 한 게시글입니다.");
+    ALREADY_LIKED(HttpStatus.BAD_REQUEST, "A200", "이미 좋아요 한 게시글입니다."),
+    CANNOT_LIKE_MY_ARTICLE(HttpStatus.BAD_REQUEST, "A201", "자신의 게시글에는 좋아요 할 수 없습니다."),
+
+    FORBIDDEN(HttpStatus.FORBIDDEN, "A300", "게시글에 권한이 없습니다."),
+    UPDATE_FORBIDDEN(HttpStatus.FORBIDDEN, "A301", "게시글 수정 권한이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
