c09711c606d35c7762bba445001a6b5408c4f870
HyeonJeong <wgwjh05169@gmail.com>
2024-09-22 15:31:40 +0900

    feat: 좋아요한 게시글 조회 API 구현

--- src/main/java/team/hackerping/nanuri/article/presentation/LikeController.java
+++ src/main/java/team/hackerping/nanuri/article/presentation/LikeController.java
@@ -4,7 +4,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import team.hackerping.nanuri.article.presentation.dto.ArticleResponse;
 
 @Tag(name = "Like", description = "좋아요 관련 API")
 public interface LikeController {
@@ -24,4 +28,16 @@ public interface LikeController {
             }
     )
     void unlikeArticle(@PathVariable Long articleId);
+
+
+    @Operation(summary = "좋아요 한 게시글 목록 조회")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "좋아요 한 게시글 목록 조회 성공"),
+            }
+    )
+
+    ResponseEntity<ArticleResponse.Paging> myLikes(
+            @ParameterObject Pageable pageable
+    );
 }
--- src/main/java/team/hackerping/nanuri/article/presentation/LikeRestController.java
+++ src/main/java/team/hackerping/nanuri/article/presentation/LikeRestController.java
@@ -1,19 +1,26 @@
 package team.hackerping.nanuri.article.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import team.hackerping.nanuri.article.application.LikeService;
+import team.hackerping.nanuri.article.application.command.PagingArticleByLikeCommand;
+import team.hackerping.nanuri.article.application.info.ArticleInfo;
+import team.hackerping.nanuri.article.presentation.dto.ArticleResponse;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/articles/{id}/likes")
+@RequestMapping("/api/articles")
 @RestController
 public class LikeRestController implements LikeController {
 
     private final LikeService likeService;
 
     @Override
-    @PostMapping
+    @PostMapping("/{id}/likes")
     public void likeArticle(@PathVariable Long id) {
 
         Long userId = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
@@ -22,11 +29,23 @@ public class LikeRestController implements LikeController {
     }
 
     @Override
-    @DeleteMapping
+    @DeleteMapping("/{id}/likes")
     public void unlikeArticle(@PathVariable Long id) {
 
         Long userId = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
 
         likeService.unlikeArticle(userId, id);
+    }
+
+    @Override
+    @GetMapping("/likes")
+    public ResponseEntity<ArticleResponse.Paging> myLikes(
+            @ParameterObject Pageable pageable
+    ) {
+        Long userId = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
+
+        ArticleInfo.Paging info = likeService.myLikes(new PagingArticleByLikeCommand(userId, pageable));
+
+        return ResponseEntity.status(HttpStatus.OK).body(ArticleResponse.Paging.of(info));
     }
 }
