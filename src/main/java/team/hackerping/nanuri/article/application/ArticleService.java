e2a347d4f55ad48b75f811e29f684960252ecb07
HyeonJeong <wgwjh05169@gmail.com>
2024-09-22 14:16:40 +0900

    feat: 애플리케이션에 게시글 수정 메소드 작성

--- src/main/java/team/hackerping/nanuri/article/application/ArticleService.java
+++ src/main/java/team/hackerping/nanuri/article/application/ArticleService.java
@@ -6,11 +6,13 @@ import org.springframework.stereotype.Service;
 import team.hackerping.nanuri.article.application.command.ChangeStatusCommand;
 import team.hackerping.nanuri.article.application.command.PagingArticleCommand;
 import team.hackerping.nanuri.article.application.command.RegisterArticleCommand;
+import team.hackerping.nanuri.article.application.command.UpdateArticleCommand;
 import team.hackerping.nanuri.article.domain.Article;
 import team.hackerping.nanuri.article.domain.ArticleImage;
 import team.hackerping.nanuri.article.persistence.ArticleJpaQueryRepository;
 import team.hackerping.nanuri.article.persistence.ArticleRepository;
 import team.hackerping.nanuri.global.exception.NanuriException;
+import team.hackerping.nanuri.global.exception.code.ArticleError;
 import team.hackerping.nanuri.global.exception.code.GeneralError;
 import team.hackerping.nanuri.user.domain.User;
 import team.hackerping.nanuri.user.persistence.UserRepository;
@@ -67,6 +69,30 @@ public class ArticleService {
 
     public Page<Article> searchArticles(PagingArticleCommand command) {
         return articleJpaQueryRepository.pagingByCriteria(command);
+    }
+
+    public Article updateArticle(UpdateArticleCommand command) {
+        Article article = articleRepository.findById(command.getArticleId())
+                .orElseThrow(() -> new NanuriException(GeneralError.NOT_FOUND, "게시글"));
+
+        if (article.getWriter().getId() != command.getUserId())
+            throw new NanuriException(ArticleError.UPDATE_FORBIDDEN, "본인의 게시글이 아닙니다.");
+
+        // TODO: S3에 업로드 및 기존 사진 삭제
+        List<String> imgUrls = List.of("aaaaaa", "bbbbbb", "cccccc");
+        ArticleImage image = ArticleImage.of(imgUrls);
+
+        article.update(
+                command.getTitle(),
+                command.getContent(),
+                command.getCategory(),
+                command.getShareType(),
+                command.getRentalStartDate(),
+                command.getRentalEndDate(),
+                image
+        );
+
+        return article;
     }
 
     public void deleteArticle(Long userId, Long articleId) {
