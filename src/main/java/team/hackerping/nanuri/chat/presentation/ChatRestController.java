6da352a188c6237bdb48825b5e65af861e56f8e5
sanghee0820 <lsh277604@gmail.com>
2024-09-22 15:40:48 +0900

    feat: 채팅 API 추가

--- src/main/java/team/hackerping/nanuri/chat/presentation/ChatRestController.java
+++ src/main/java/team/hackerping/nanuri/chat/presentation/ChatRestController.java
@@ -1,38 +1,67 @@
 package team.hackerping.nanuri.chat.presentation;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import team.hackerping.nanuri.chat.application.ChatService;
 import team.hackerping.nanuri.chat.presentation.dto.ChatRequest;
+import team.hackerping.nanuri.chat.presentation.dto.ChatRequest.Create;
 import team.hackerping.nanuri.chat.presentation.dto.ChatResponse.Paging;
 import team.hackerping.nanuri.chat.presentation.dto.ChatResponse.RoomDetail;
 
 @RestController
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/api/chat")
 public class ChatRestController implements ChatController {
+    private final ChatService chatService;
 
     @Override
     @PostMapping()
-    public void sendMessage(ChatRequest.SendMessage request) {
-        //Todo
+    public ResponseEntity<Long> createRoom(@RequestBody Create request,
+                                           Authentication authentication
+    ) {
+        var userId = Long.valueOf(authentication.getPrincipal().toString());
+        var command = request.toCommand(userId);
+        var roomId = chatService.createRoom(command);
+
+        return ResponseEntity.ok()
+                .body(roomId);
+    }
+
+    @Override
+    @PostMapping("/message")
+    public void sendMessage(@RequestBody ChatRequest.SendMessage request,
+                            Authentication authentication
+    ) {
+        var userId = Long.valueOf(authentication.getPrincipal().toString());
+        var command = request.toCommand(userId);
+        chatService.sendMessage(command);
     }
 
     @Override
     @GetMapping()
-    public ResponseEntity<Paging> getRooms(
-            @PageableDefault Pageable pageable
+    public ResponseEntity<Paging> getRooms(@PageableDefault Pageable pageable,
+                                           Authentication authentication
     ) {
         return null;
     }
 
     @Override
     @GetMapping("/{roomId}")
-    public ResponseEntity<RoomDetail> getMessage(@PathVariable Long roomId) {
+    public ResponseEntity<RoomDetail> getMessage(@PathVariable Long roomId,
+                                                 Authentication authentication
+
+    ) {
         return null;
     }
 }
--- src/main/java/team/hackerping/nanuri/chat/presentation/dto/ChatRequest.java
+++ src/main/java/team/hackerping/nanuri/chat/presentation/dto/ChatRequest.java
@@ -1,9 +1,23 @@
 package team.hackerping.nanuri.chat.presentation.dto;
 
+import team.hackerping.nanuri.chat.application.dto.ChatMessageCommand;
+import team.hackerping.nanuri.chat.application.dto.ChatRoomCommand;
+
 public class ChatRequest {
+    public record Create(
+            Long articleId
+    ) {
+        public ChatRoomCommand.Create toCommand(Long userId) {
+            return new ChatRoomCommand.Create(articleId, userId);
+        }
+    }
+
     public record SendMessage(
-            Long receiverId,
-            Long articleId,
+            Long roomId,
             String message
-    ) { }
+    ) {
+        public ChatMessageCommand.Create toCommand(Long userId) {
+            return new ChatMessageCommand.Create(roomId, userId, message);
+        }
+    }
 }
