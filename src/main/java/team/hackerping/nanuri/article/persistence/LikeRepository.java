6bee0edf27aa45402a6434d146c7c83c6f3aea7a
HyeonJeong <wgwjh05169@gmail.com>
2024-09-22 12:10:00 +0900

    feat: LikeRepository에 좋아요가 존재하는지 확인하는 메소드 작성

--- src/main/java/team/hackerping/nanuri/article/persistence/LikeRepository.java
+++ src/main/java/team/hackerping/nanuri/article/persistence/LikeRepository.java
@@ -1,7 +1,9 @@
 package team.hackerping.nanuri.article.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import team.hackerping.nanuri.article.domain.Article;
 import team.hackerping.nanuri.article.domain.Like;
+import team.hackerping.nanuri.user.domain.User;
 
 import java.util.List;
 
@@ -9,4 +11,6 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
     Boolean existsByUserIdAndArticleId(Long userId, Long articleId);
 
     List<Like> findAllByUserId(Long userId);
+
+    Boolean existsByUserAndArticle(User user, Article article);
 }
